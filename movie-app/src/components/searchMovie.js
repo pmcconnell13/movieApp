b82ebdf7e546a81bc00035bf4f14a382712e6a1e
Patrick McConnell <pmcconnell13@gmail.com>
2021-07-20 15:59:14 -0700

    add cards

--- movie-app/src/components/searchMovie.js
+++ movie-app/src/components/searchMovie.js
@@ -1,10 +1,46 @@
+import { useState } from 'react';
+
 export default function SearchMovie(){
 
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=4fd50d9704b2f645d20747e085895b5a&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data  = await res.json();
+      setMovies(data.results)
+      console.log(data);
+  }catch(err){
+      console.error(err);
+  }
+  console.log('movies', movies)
+  }
+
   return (
-        <form className="form">
-          <label htmlFor="query">Movie Name</label>
-          <input type="text" name="query" placeholder="searchMe" />
+      <>
+        <form className="form" onSubmit={searchMovies}>
+          <label className="label" htmlFor="query">Movie Name</label>
+          <input className="input" type="text" name="query" placeholder="searchMe" value={query} onChange={(e) => setQuery(e.target.value)} />
           <button className="button" type="submit">Search</button>
         </form>
+        <div className="card-list">
+          {movies.filter(movie => movie.poster_path).map(movie => (
+            <div className="card" key={movie.id}>
+              <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + 'poster'} />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p><small>RELEASE DATE: {movie.realse_date}</small></p>
+                <p><small>RATING: {movie.vote_average}</small></p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
   )
 }
